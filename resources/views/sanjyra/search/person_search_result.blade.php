c2ee2c9fc5e25c84d5d68f81a1e721428d31b76b
Aziz <azizalmazbekuulu@gmail.com>
2020-03-09 21:45:17 +0600

    search result display fix

--- resources/views/sanjyra/search/person_search_result.blade.php
+++ resources/views/sanjyra/search/person_search_result.blade.php
@@ -35,60 +35,42 @@
             </tr>
         @endforelse
         </tbody>
-        {{-- <tfoot>
-            <tr>
-                <td colspan="3">
-                    <ul class="pagination float-right">
-                        {{$men->links()}}
-                    </ul>
-                </td>
-            </tr>
-        </tfoot> --}}
     </table>
     </div>
-    @else
+    @endif
+<div class="col-sm-6">
+    @if ($men != null)
+    <table>
+        <thead>
+            <th><div>№</div></th>
+            <th><div>Чоң атасы</div></th>
+            <th><div>Атасы</div></th>
+            <th><div>Өзү</div></th>
+            <th><div>Уруусу</div></th>
+        </thead>
+        <tbody>
+        @forelse ($men as $man)
+        <tr>
+            <td>{{$i++}}</td>
+            <td><a href="{{route('man', $man->grand_id)}}">{{ $man->grand_name }}</a></td>
+            <td><a href="{{route('man', $man->father_id)}}">{{ $man->father_name }}</a></td>
+            <td><a href="{{route('man', $man->id)}}">{{ $man->name }}</a></td>
+            <td>{{ $man->uruusu }}</td>
+        </tr>
+        @empty
+            <tr>
+                <td colspan="3" class="text-center">Макалалар жок</td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
+    @elseif ($women == null)
     <ul>
         <li>Аты: {{$name}}</li>
         <li>Атасынын аты: {{$father_name}}</li>
     </ul>
     <p>Мындай маалымат табылган жок. Маалыматтын тууралыгын текшериңиз.</p>
     @endif
-<div class="col-sm-6">
-@if ($men != null)
-<table>
-    <thead>
-        <th><div>№</div></th>
-        <th><div>Чоң атасы</div></th>
-        <th><div>Атасы</div></th>
-        <th><div>Өзү</div></th>
-        <th><div>Уруусу</div></th>
-    </thead>
-    <tbody>
-    @forelse ($men as $man)
-    <tr>
-        <td>{{$i++}}</td>
-        <td><a href="{{route('man', $man->grand_id)}}">{{ $man->grand_name }}</a></td>
-        <td><a href="{{route('man', $man->father_id)}}">{{ $man->father_name }}</a></td>
-        <td><a href="{{route('man', $man->id)}}">{{ $man->name }}</a></td>
-        <td>{{ $man->uruusu }}</td>
-    </tr>
-    @empty
-        <tr>
-            <td colspan="3" class="text-center">Макалалар жок</td>
-        </tr>
-    @endforelse
-    </tbody>
-    {{-- <tfoot>
-        <tr>
-            <td colspan="3">
-                <ul class="pagination float-right">
-                    {{$men->links()}}
-                </ul>
-            </td>
-        </tr>
-    </tfoot> --}}
-</table>
-@endif
-</div>
+    </div>
 </div>
 @endsection
